2cea18532d84b457645ec1e159b98e7aa6aa4b5a
Matthias Kümmerer <matthias@matthias-k.org>
2015-10-07 14:42:07 -0400

    Setup.py
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gitdb2',
+      version='0.1.0',
+      description='store sqlalchemy database in git repository',
+      author='Matthias Kümmerer',
+      author_email='matthias@matthias-k.org',
+      url='https://github.com/matthias-k/gitdb2',
+      packages=['gitdb2'],
+     )
