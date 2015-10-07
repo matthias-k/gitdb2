5fecd5221ac6db8c429b7cb98d9614e3d05ceb26
Matthias Kümmerer <matthias@matthias-k.org>
2015-10-07 14:49:39 -0400

    Remove unicode
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(name='gitdb2',
       version='0.1.0',
       description='store sqlalchemy database in git repository',
-      author='Matthias Kümmerer',
+      author='Matthias Kuemmerer',
       author_email='matthias@matthias-k.org',
       url='https://github.com/matthias-k/gitdb2',
       packages=['gitdb2'],
