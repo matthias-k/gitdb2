721ad252c85192769c5d2679db6df8dff0f3afcc
Matthias Kümmerer <matthias@matthias-k.org>
2016-01-05 13:56:24 +0100

    Version bump
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='gitdb2',
-      version='0.1.0',
+      version='0.2.0',
       description='store sqlalchemy database in git repository',
       author='Matthias Kuemmerer',
       author_email='matthias@matthias-k.org',
